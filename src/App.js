94fd148cfc2856649205d4ff62921e1bbcf0d001
Thay Mendes <thay.gm@live.com>
2023-10-13 11:17:46 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigationbar from "./components/Navbar";
 import PublicRoute from "./routes/PublicRoutes";
-import MedicineSearch from "./components/MedicineSeach";
+
 
 function App() {
   return (
